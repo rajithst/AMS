eeb5f7a5af32ee1d0317d4148cf7d980bf3fa387
Emalsha <remalsha@gmail.com>
2016-09-13 23:48:18 +0530

    sample

---
+++ application/controllers/Home.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Class for home view
+ */
+class Home extends CI_Controller
+{
+
+  public function getHome()
+  {
+    $this->load->view('home');
+  }
+
+}
+
+
+ ?>
---
+++ application/views/home.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>ANS | HOME</title>
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.css'); ?> ">
+  </head>
+  <body>
+      <h1>Hello world</h1>
+  </body>
+</html>

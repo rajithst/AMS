f41071b193341194d687bfa36017697b6619dafd
Emalsha <remalsha@gmail.com>
2016-10-22 00:03:13 +0530

    migration changes

--- application/migrations/013_asset_register.php
+++ application/migrations/013_asset_register.php
@@ -150,19 +150,19 @@ class Migration_Asset_register extends CI_Migration {
 				),
 
 			));
-		$this->dbforge->add_key('id', TRUE);
-		$this->dbforge->add_field('CONSTRAINT FOREIGN KEY (asset_category) REFERENCES Asset_category(cat_id)');
-		$this->dbforge->add_field('CONSTRAINT FOREIGN KEY (sub_category) REFERENCES Asset_sub_category(sub_cat_id)');
-		$this->dbforge->add_field('CONSTRAINT FOREIGN KEY (asset_custodian) REFERENCES Asset_custodian(custodian_id)');
-		$this->dbforge->add_field('CONSTRAINT FOREIGN KEY (asset_owner) REFERENCES Asset_owner(owner_id)');
-		$this->dbforge->add_field('CONSTRAINT FOREIGN KEY (asset_model) REFERENCES Asset_model(model_id)');
-		$this->dbforge->add_field('CONSTRAINT FOREIGN KEY (manufacturer) REFERENCES Asset_manufacture(manufacture_id)');
-		$this->dbforge->add_field('CONSTRAINT FOREIGN KEY (location) REFERENCES Asset_location(location_id)');
-		$this->dbforge->add_field('CONSTRAINT FOREIGN KEY (asset_classification) REFERENCES Asset_classification(classification_id)');
-		$this->dbforge->add_field('CONSTRAINT FOREIGN KEY (lifetime_period) REFERENCES Asset_lifetime(lifetime_id)');
-		$this->dbforge->add_field('CONSTRAINT FOREIGN KEY (warrenty_period) REFERENCES Asset_warranty(warranty_id)');
-		$this->dbforge->add_field('CONSTRAINT FOREIGN KEY (vendor) REFERENCES Asset_vendor(vendor_id)');
-		$this->dbforge->add_field('CONSTRAINT FOREIGN KEY (dataInputer) REFERENCES user(id)');
+		$this->dbforge->add_key('asset_id', TRUE);
+		$this->dbforge->add_field('CONSTRAINT FOREIGN KEY (asset_category) REFERENCES Asset_category(cat_id)  ON DELETE CASCADE');
+		$this->dbforge->add_field('CONSTRAINT FOREIGN KEY (asset_sub_category) REFERENCES Asset_sub_category(sub_cat_id)  ON DELETE CASCADE');
+		$this->dbforge->add_field('CONSTRAINT FOREIGN KEY (asset_custodian) REFERENCES Asset_custodian(custodian_id)  ON DELETE CASCADE');
+		$this->dbforge->add_field('CONSTRAINT FOREIGN KEY (asset_owner) REFERENCES Asset_owner(owner_id) ON DELETE CASCADE');
+		$this->dbforge->add_field('CONSTRAINT FOREIGN KEY (asset_model) REFERENCES Asset_model(model_id) ON DELETE CASCADE');
+		$this->dbforge->add_field('CONSTRAINT FOREIGN KEY (asset_manufacturer) REFERENCES Asset_manufacture(manufacture_id) ON DELETE CASCADE');
+		$this->dbforge->add_field('CONSTRAINT FOREIGN KEY (asset_location) REFERENCES Asset_location(location_id) ON DELETE CASCADE');
+		$this->dbforge->add_field('CONSTRAINT FOREIGN KEY (asset_classification) REFERENCES Asset_classification(classification_id) ON DELETE CASCADE');
+		$this->dbforge->add_field('CONSTRAINT FOREIGN KEY (lifetime_period) REFERENCES Asset_lifetime(lifetime_id) ON DELETE CASCADE');
+		$this->dbforge->add_field('CONSTRAINT FOREIGN KEY (warrenty_period) REFERENCES Asset_warranty(warranty_id) ON DELETE CASCADE');
+		$this->dbforge->add_field('CONSTRAINT FOREIGN KEY (asset_vendor) REFERENCES Asset_vendor(vendor_id) ON DELETE CASCADE');
+		$this->dbforge->add_field('CONSTRAINT FOREIGN KEY (dataInputer) REFERENCES user(id) ON DELETE CASCADE');
 
 		//loation relationship
 		$this->dbforge->create_table('Asset_Details');
